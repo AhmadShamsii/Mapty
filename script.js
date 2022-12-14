a133fa746da47eae0fc348f6359ae9e09fe42232
AhmadShamsii <ahmaddshamsii@gmail.com>
2022-12-14 14:31:13 +0500

    bugss fixd

--- script.js
+++ script.js
@@ -113,24 +113,11 @@ class App {
     const coords = [latitude, longitude];
     console.log('final');
 
-    var map = L.map('map').setView([51.505, -0.09], 13);
-
     this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution:
-        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(this.#map);
-
-    // L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    // '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    // }).addTo(this.#map);
-
-    attribution: L.marker(coords)
-      .addTo(map)
-      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-      .openPopup();
 
     // Handling clicks on map
     this.#map.on('click', this._showForm.bind(this));
