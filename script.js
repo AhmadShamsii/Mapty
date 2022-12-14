551bc9f622f533b50f033eb00e09037872094b42
AhmadShamsii <ahmaddshamsii@gmail.com>
2022-12-14 14:19:19 +0500

    bugss fixd

--- script.js
+++ script.js
@@ -118,8 +118,14 @@ class App {
     this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     }).addTo(this.#map);
+
+    // L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    // '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    // }).addTo(this.#map);
 
     attribution: L.marker(coords)
       .addTo(map)
